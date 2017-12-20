f66d86026f34f34007b5dd1a94b275eadf0a5914
abugi <abugiboi@gmail.com>
2017-12-20 16:23:15 +0100

    initial commit

--- public/chat_me.js
+++ public/chat_me.js
@@ -1,7 +1,1 @@
-const socket = io.connect();
 
-const button = document.querySelector('button');
-
-button.addEventListener('click', function(){
-        socket.emit('greeting', {greet: 'Hello everyone'});
-});
