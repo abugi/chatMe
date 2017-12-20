ddf614ec4fd3026ab08c81e4adcb33e201894241
abugi <abugiboi@gmail.com>
2017-12-20 18:00:50 +0100

    initial commit

--- public/chat_me.js
+++ public/chat_me.js
@@ -1,9 +1,7 @@
 const socket = io.connect();
 
-const newData = document.querySelector('#newData');
-
 socket.emit('greet', {say: 'Hello everybody', question: 'Whats going on in the house?'});
 
 socket.on('greet', function(data){
-    newData.innerHTML = data.say;
+    
 });
