c9846ba61c724ee2e709432291aaf1eb3e47cdfd
abugi <abugiboi@gmail.com>
2017-12-20 16:57:11 +0100

    initial commit

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ const io = socket(server);
 
 io.on('connection', function(socket){
     socket.on('greet', function(data){
-        console.log(data.say);
+        io.sockets.emit(data);
     });
 });
--- public/chat_me.js
+++ public/chat_me.js
@@ -1,3 +1,9 @@
 const socket = io.connect();
 
+const data = document.querySelector('#data');
+
 socket.emit('greet', {say: 'Hello everybody'});
+
+socket.on('greet', function(data){
+    data.innerHTML = data.say;
+});
