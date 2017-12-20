cb802f8da8680b2c17e764d2ff71f9a0c8f02882
abugi <abugiboi@gmail.com>
2017-12-20 15:21:13 +0100

    initial commit

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const   express = require('express'),
         socket=  require('socket.io'),
         app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
     res.send('We are live!!');
 });
 
-app.listen(3030, function(){
+const server = app.listen(3030, function(){
     console.log('chat is live on port 3030');
+});
+
+const io = socket(server);
+
+io.on('connection', function(data){
+    console.log('connection initiated');
 });
